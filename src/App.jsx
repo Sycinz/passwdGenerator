5f797f673e37e6f18ae24cf58989c032be18222a
Sycinz <gronbosof@gmail.com>
2024-01-07 10:43:58 +0100

    Basic site schema

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { useCallback, useState } from 'react'
 import './App.scss'
+import './components/Pass'
 
-function App() {
+const [result, setResult] = useState('')
+const [inputVal, setInputVal] = useState('')
+
+const handleChange = (event) => {
+  const { value } = event.target
+
+  if (value < 0 && value > 100) return null 
+
+  setInputVal(prevVal => value)
+}
+
+const App = () => {
 
   return (
-    <div></div>
+    <div className='container'>
+      <h1 className='container__title'>Password Generator</h1>
+      <input className="" type="range" min={0} max={100} value={inputVal} onChange={handleChange}/>
+
+    </div>
   )
 }
 
---
+++ src/components/Pass.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react"
+
+const passGenerate = (length, numbers, symbols, spaces) => {
+    
+
+
+    return result
+}
