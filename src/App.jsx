6df7f72d048a87e265dcebfd2772d306c6f0cbf7
Sycinz <gronbosof@gmail.com>
2024-01-07 10:46:17 +0100

    quick fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import { useCallback, useState } from 'react'
+import { useState } from 'react'
 import './App.scss'
 import './components/Pass'
 
-const [result, setResult] = useState('')
-const [inputVal, setInputVal] = useState('')
-
-const handleChange = (event) => {
-  const { value } = event.target
-
-  if (value < 0 && value > 100) return null 
-
-  setInputVal(prevVal => value)
-}
-
 const App = () => {
+  const [result, setResult] = useState('')
+  const [inputVal, setInputVal] = useState('')
+  
+  const handleChange = (event) => {
+    const { value } = event.target
+  
+    if (value < 0 && value > 100) return null 
+  
+    setInputVal(prevVal => value)
+  }
 
   return (
     <div className='container'>
